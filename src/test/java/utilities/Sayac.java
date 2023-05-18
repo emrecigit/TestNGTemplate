c338317f0b529b70ac081b564ee79974ee784f1a
Emre Cigit <125315547+emrecigit@users.noreply.github.com>
2023-05-18 23:23:57 +0300

    2305182323

---
+++ src/test/java/utilities/Sayac.java
@@ -0,0 +1,22 @@
+package utilities;
+
+import org.testng.annotations.Test;
+
+import java.time.LocalTime;
+
+public class Sayac {
+    @Test
+    public void Sayac(){
+        // x saniyelik bir sayac yapalim
+        int x=5;
+        LocalTime baslangic = LocalTime.now();
+        int basSaniye = baslangic.getSecond();
+        int saniyeKontrol = 123;
+        int bitisSaniyesi = basSaniye+3>60 ? basSaniye+x-60 : basSaniye+x;
+        while (bitisSaniyesi!= saniyeKontrol){
+            saniyeKontrol = LocalTime.now().getSecond();
+        }
+        System.out.println("baslangic saniyesi : "+basSaniye);
+        System.out.println("saniyeKontrol : "+saniyeKontrol);
+    }
+}
